22c394a36e9be1ea9d9d1990bfd0f5ee0b213f3d
Zichao Yang <zichaoy@cs.cmu.edu>
2018-11-13 14:22:19 -0500

    rewrite run.sh

--- run.sh
+++ run.sh
@@ -1,44 +1,61 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=MIXER_run
+#SBATCH --gres=gpu:1
+#SBATCH --mem=30g
+#SBATCH -c 8
+#SBATCH --time 0
+#SBATCH -o .log.MIXER_run
+
+#set -x  # echo commands to stdout
+#set -e  # exit on error
+
+if [[ $(hostname -s) = compute-* ]]; then
+	module load cuda-91
+	module load cudnn-9.1-7.1
+	source ${HOME}/.bashrc
+fi
+
+model_name=mle_rl_bleu
+load_model_name=mle_update
+
+data_config="
+    --train_src data/iwslt14/train.de
+    --train_tgt data/iwslt14/train.en
+    --dev_src data/iwslt14/valid.de
+    --dev_tgt data/iwslt14/valid.en
+    --test_src data/iwslt14/test.de
+    --test_tgt data/iwslt14/test.en
+    --vocab vocab.bin"
+batch_size=80
+decode_max_time_step=50
+log_every=1000
+valid_niter=1916
 
 python nmt.py \
-    --train_src data/iwslt14/train.de \
-    --train_tgt data/iwslt14/train.en \
-    --dev_src data/iwslt14/valid.de \
-    --dev_tgt data/iwslt14/valid.en \
-    --test_src data/iwslt14/test.de \
-    --test_tgt data/iwslt14/test.en \
-    --vocab vocab.bin \
-    --decode_max_time_step 60 \
-    --batch_size 64 \
+	${data_config} \
     --model_type ml \
+    --batch_size ${batch_size} \
     --dropout 0.2 \
-    --beam_size 5 \
-    --log_every 1 \
-    --save_to mle_model_for_mixer \
-    --save_to_file test_result.txt \
-    --mode train \
-    --valid_niter 2394 \
-    --max_nepoch 20
-
+    --beam_size 1 \
+    --decode_max_time_step ${decode_max_time_step} \
+    --log_every ${log_every} \
+    --valid_niter ${valid_niter} \
+    --save_to mle_model \
+    --save_to_file mle_test_result.txt \
+    --max_nepoch 20 \
+	--cuda
 
 python nmt.py \
-    --train_src data/iwslt14/train.de \
-    --train_tgt data/iwslt14/train.en \
-    --dev_src data/iwslt14/valid.de \
-    --dev_tgt data/iwslt14/valid.en \
-    --test_src data/iwslt14/test.de \
-    --test_tgt data/iwslt14/test.en \
-    --vocab ./vocab.bin \
-    --batch_size 64 \
-    --beam_size 5 \
-    --dropout 0.2 \
-    --decode_max_time_step 60 \
+	${data_config} \
     --model_type mixer \
-    --valid_niter 2394 \
-    --valid_metric 'bleu' \
-    --log_every 1000 \
-    --save_to mixer_model \
     --reward_type "bleu" \
+    --batch_size ${batch_size} \
+    --dropout 0.2 \
+    --beam_size 1 \
+    --decode_max_time_step ${decode_max_time_step} \
+    --log_every ${log_every} \
+    --valid_niter ${valid_niter} \
+    --save_to mixer_model \
+    --save_to_file mixer_test_result.txt \
     --load_model mle_model_for_mixer.bin \
     --cuda
-
